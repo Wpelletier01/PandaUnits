de54bc07ce56632c26fb02917f7d778327ea2137
user <wpelletier.development@yahoo.com>
2023-05-29 11:43:39 -0400

    add icon to each html pages

--- website/js/weight.js
+++ website/js/weight.js
@@ -1,3 +1,39 @@
 
 
-export function convert();
+export function convert(input,iunit,runit) {
+
+    var result = 0;
+
+    if (input == "0" || input == ".") {
+
+        return result;
+
+    } 
+
+
+    if (IMPERIAL.includes(iunit) && (METRIC.includes(runit))) {
+        
+     
+
+    } else if (IMPERIAL.includes(iunit) && IMPERIAL.includes(runit)) {
+
+        
+    
+    } else if (METRIC.includes(iunit) && IMPERIAL.includes(runit)) {
+
+    
+    } else if (METRIC.includes(iunit) && METRIC.includes(runit)) {
+
+     
+
+    } else {
+        // TODO: handling error
+        return 0;
+    }
+
+
+    return result;
+
+
+
+}
