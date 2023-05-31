043b358707e18ccbcd25a943af3cf554af94786b
user <wpelletier.development@yahoo.com>
2023-05-30 20:09:35 -0400

    start adding comment to the code

--- website/js/script.js
+++ website/js/script.js
@@ -1,9 +1,12 @@
 
+// will store the section.json file
 var data = {};
-
+// html page name
 var pname = getCurrentPage();
+// store the extern script depending the name of the file that load this script
 let module;
 
+// make sure that is not the index file
 if ( pname != null && pname != "index") {
 
     var moduleFile = './' + pname + '.js';
@@ -12,21 +15,26 @@ if ( pname != null && pname != "index") {
 
 }
 
+/*
 
 
+Jquery section
 
 
-// when page load
+*/
+
+// when the page is load and ready
 $(function() {
 
+    // set the year on the fake legal notice message 
     var d = new Date().getFullYear();
-    
     $("#legal").text("© " + d + " Pandas Units. No right reserved");
 
+    // make sure that the json file is only load in page that need it
     if (pname != null && pname != "index") {
 
         $.getJSON("../json/section.json",function(section) {
-            var d = new Date();
+         
             data = section[pname];
             init();
         });
@@ -37,7 +45,9 @@ $(function() {
 
 });
 
-// when the invert button is trigger
+/*
+*   take the value selected from the "from" dropdown and make it selected on the "to" dropdown, vice versa
+*/
 $("#invert").on("click", function() {
       
                 
@@ -52,11 +62,18 @@ $("#invert").on("click", function() {
 
 });
 
+
+/*
+*   Trigger state change when the "From" dropdown change of selection
+*/
 $("#from-select").on("change",function() { 
 
-
+    // capture the new value
     var from = $("#from-select").val();
     
+    // for all the other type of conversion,
+    // we need to make sure that if the user change the type of unit (eg. Imperial to Metric)
+    // because if so, we change the vtype attribute 
     if (pname != "time" && pname != "temparature") {
 
         if (module.IMPERIAL.includes(from)) {
@@ -69,24 +86,27 @@ $("#from-select").on("change",function() {
     
         }
     
-
-
     } 
 
+    // This is for when a conversion have already been made, we want that another conversion
+    // trigger immediately 
     if (getFromVal() != "0") {
 
         convertTrigged();
 
     } else {
-
+        // since convertTrigged() call updateDisplayUnit(), we dont want to call a second time
         updateDisplayUnit(); 
 
     }
 
 
-
 }); 
 
+
+/*
+*   Trigger state change when the "To" dropdown change of selection
+*/
 $("#to-select").on("change",function(){
     
     
@@ -120,9 +140,10 @@ $("#to-select").on("change",function(){
 
 });
 
-
+/*
+*   Trigger the conversion by pressing the Enter key
+*/
 $(document).on("keypress", function(e) {
-
 
     if (e.key == "Enter") {
 
@@ -130,40 +151,50 @@ $(document).on("keypress", function(e) {
 
     }
 
-
 });
 
-
+/*
+*   Trigger the conversion by clicking the button "convert"
+*/
 $("#bconvert").on("click",function() { convertTrigged()});
 
+/*
+*   Reset the value to zero when the button reset is trigger
+*/
 $("#breset").on("click",function() {
-
-
 
     $("#from-input").val("0");
     $("#result").val("0");
 
-
-
-
 });
 
+/*
+
+    Other function
+
+*/
 
 
+/*
+*   convert the from value to the "to" unit 
+*/
 function convertTrigged() {
 
+    // store the value from entered
     var input = $("#from-input").val();
 
+    // make sure is a legal input
     if (validateFromInput(input)){
 
+        // call the "abstract" function 
         var result = module.convert(input,getFromUnit(),getToUnit());
 
+        // for floating number, we dont want more than 5 decimals points 
         if(!Number.isInteger(result)){
 
             // TODO: convert to scientific anotation when number is too small or too big
 
             result = round(result,5);
-
 
         }
         
@@ -176,31 +207,28 @@ function convertTrigged() {
 
 }
 
-
+/*
+*   initialise the value after loading the json file 
+*/
 function init() {
 
-    
-
+    // temparature and time unit are not seperate by imperial or metric
     if (pname == "temparature" || pname == "time") {
     
         setSelectionVal("#from-select","units");
         setSelectionVal("#to-select","units",true);
-
+        // initialise the value
         $("#from-input").val("0");
         $("#result").val("0");
 
     } else {
 
-
-
-
+        // set one type of values after the other
         setSelectionVal("#from-select","imperial");
         setSelectionVal("#from-select","metric");
-
         setSelectionVal("#to-select","metric");
         setSelectionVal("#to-select","imperial");
-
-
+        // initialise the value
         $("#from-input").val("0");
         $("#result").val("0");
 
